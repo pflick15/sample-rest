ddea8ffa8cc34f15c3774730834e2b84481fd118
Paul Flick <pflick34@gmail.com>
2017-07-18 22:26:15 -0400

    initial commit

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,32 @@
+package hello;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World5") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+	@RequestMapping("/service-instances/{applicationName}")
+	public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
+		return this.discoveryClient.getInstances(applicationName);
+	}
+}
